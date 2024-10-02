ab661f8086f9d1f59149b4f3e173c61d9ed914ff
klmadhu247 <klmadhu0709@gmail.com>
2024-10-02 15:57:36 +0530

    updated with final code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Quiz_Game from "./Quiz_Game";
+
+function App() {
+  return (
+    <div>
+    <Quiz_Game/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Quiz_Game.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Quiz.css'
+import { QuizData } from "./data";
+
+
+function Quiz_Game() {
+    
+    const questionsList = QuizData;
+
+    const [questionCount,setQuestionCount] = useState(0)
+    const [showScore,setShowScore] = useState(false);
+    const [score,setScore] = useState(0);
+    const [selectedOption,setSelectedOption] = useState(0)
+
+
+    const handleClick=()=>
+    {
+        if(questionCount<questionsList.length-1){
+            setQuestionCount(questionCount+1)
+        }
+        else{
+            setShowScore(true)
+        }
+    }
+
+    const handleTryAgain=()=>
+    {
+        setQuestionCount(0)
+        setShowScore(false)
+    }
+
+    const handleAnswer=(index)=>
+    {
+        setSelectedOption(index)
+        if(index+1 === questionsList[questionCount].answer)
+        {
+            setScore(score+1)
+
+        }
+
+    }
+
+
+
+    return (
+        <div style={{ backgroundColor: '#ab6fe0', height: '100vh' }}>
+            <div className="container">
+                <h3 style={{color:"white"}}>QUIZ APP</h3>
+                
+                {!showScore && <div className="container_box">
+                    <div className="question">{questionsList[questionCount].question}</div>
+                   
+                   {questionsList[questionCount].options.map((ql,index)=><li  onClick={()=>handleAnswer(index)}>{ql}</li>)} 
+                   <button onClick={handleClick} className="btn btn-primary btn-lg">
+                    {questionCount==questionsList.length? 'Try Again': 'Next'}</button> 
+                   </div> }
+
+                   {showScore && <div className="container_box"><h3>Your Score:{score} <br/>Total Score:{questionsList.length}</h3>
+                   <button onClick={handleTryAgain} className="btn btn-warning btn-sm">Try Again</button></div>}
+                   
+            </div>
+
+            
+        </div>
+    );
+}
+
+export default Quiz_Game;
